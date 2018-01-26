227750ac306006473eeddc204dddd270d5e03ff0
Armag3ddon <Armag3ddon@web.de>
2018-01-26 21:26:27 +0100

    bg

--- js/scenes/play.js
+++ js/scenes/play.js
@@ -1,9 +1,11 @@
-define(['lib/scene', 'geo/v2'],
-		function(Scene, V2) {
+define(['lib/scene', 'geo/v2', 'core/graphic'],
+		function(Scene, V2, g) {
+			g.add('img/PlayScene.jpg');
+
 			function PlayScene() {
 				Scene.call(this);
 
-
+				this.bg = 'img/PlayScene.jpg';
 			}
 
 			PlayScene.prototype = new Scene();
