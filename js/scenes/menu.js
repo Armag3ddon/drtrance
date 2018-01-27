ce44f1c4be8d2fa2013b485afab9a70a1d97f26a
MystaraCorvus <derdenker@live.de>
2018-01-28 00:30:26 +0100

    Buttonfixed

--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -10,24 +10,24 @@ define(['lib/scene', 'basic/button', 'core/graphic', 'core/game', 'geo/v2', 'tra
 		function MenuScene() {
 			Scene.call(this);
 
-			var playButton = Button.create(new V2(0, 680), function() { game.scene = require('config/scenes').play; }).img('img/button_start_normal.png').imgHover('img/button_start_hover.png');
-			var creditsButton = Button.create(new V2(0, 680), function() { game.scene = new SlideInRightTransition(require('config/scenes').credits, 1000, Easing.OUTQUAD); }).img('img/button_credits_normal.png').imgHover('img/button_credits_hover.png');
-			var helpButton = Button.create(new V2(0, 680), function() { game.scene = require('config/scenes').help; }).img('img/button_help_normal.png').imgHover('img/button_help_hover.png');
+			var playButton = Button.create(new V2(490, 330), function() { game.scene = require('config/scenes').play; }).img('img/button_start_normal.png').imgHover('img/button_start_hover.png');
+			var creditsButton = Button.create(new V2(490, 440), function() { game.scene = new SlideInRightTransition(require('config/scenes').credits, 1000, Easing.OUTQUAD); }).img('img/button_credits_normal.png').imgHover('img/button_credits_hover.png');
+			var helpButton = Button.create(new V2(490, 550), function() { game.scene = require('config/scenes').help; }).img('img/button_help_normal.png').imgHover('img/button_help_hover.png');
 
-			var vLayout = new Layout.vertical(new V2(0, 20), 20, 50);
-			vLayout.add(playButton);
-			vLayout.add(creditsButton);
-			vLayout.add(helpButton);
-			vLayout.align("center");
-			this.center(vLayout);
+			this.add(playButton);
+			this.add(creditsButton);
+			this.add(helpButton);
+
 			this.bg = 'img/main_menu_bg.jpg';
 
+			/*
 			var easing = Easing.OUTELASTIC;
 			var self = this;
 
-			playButton.add(new Morph({ position: { y: 325 } }, 2500, easing));
+			playButton.add(new Morph({ position: { y: 300 } }, 2500, easing));
 			creditsButton.add(new Morph({ position: { y: 425 } }, 2500, easing));
-			helpButton.add(new Morph({ position: { y: 525 } }, 2500, easing));
+			helpButton.add(new Morph({ position: { y: 550 } }, 2500, easing));
+			*/
 		}
 
 		MenuScene.prototype = new Scene();
