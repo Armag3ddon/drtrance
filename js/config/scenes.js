e684290b155545c40c12b460a776ed3b69511300
andre.voigt <andre.voigt@innogames.com>
2018-01-27 17:09:06 +0100

    Remove particles

--- js/config/scenes.js
+++ js/config/scenes.js
@@ -1,12 +1,11 @@
-define(['scenes/menu', 'scenes/credits', 'scenes/play', 'scenes/help', 'scenes/particles'],
-		function (MenuScene, CreditsScene, PlayScene, HelpScene, ParticleScene) {
+define(['scenes/menu', 'scenes/credits', 'scenes/play', 'scenes/help'],
+		function (MenuScene, CreditsScene, PlayScene, HelpScene) {
 			return {
 				init: function () {
 					this.menu = new MenuScene();
 					this.credits = new CreditsScene();
 					this.play = new PlayScene();
 					this.help = new HelpScene();
-					this.particles = new ParticleScene();
 					this.default = this.menu;
 				}
 			};
