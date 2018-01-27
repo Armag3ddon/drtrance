e2402edb471a03ff109e4144e43f6c081d6f8220
MystaraCorvus <derdenker@live.de>
2018-01-27 20:37:32 +0100

    Fonts Und Effekts

--- js/config/fonts.js
+++ js/config/fonts.js
@@ -1,8 +1,8 @@
 define(['definition/font'], function(FontStyle) {
 	return {
-		default: new FontStyle(40, '#000', 'sans-serif', '#555' ),
+		default: new FontStyle(40, '#000', 'Georgia', '#555' ),
 		frames: new FontStyle(12, '#000', 'monospace' ),
-		defaultRight: new FontStyle(40, '#000', 'sans-serif', '#555',  'right'),
-		defaultLeft: new FontStyle(40, '#000', 'sans-serif', '#555',  'left')
+		defaultRight: new FontStyle(40, '#000', 'Georgia', '#555',  'right'),
+		defaultLeft: new FontStyle(40, '#000', 'Georgia', '#555',  'left')
 	};
 });
