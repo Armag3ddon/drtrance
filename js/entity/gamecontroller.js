898ef7f8d15352125514e0fc336f741b7885b2a6
andre.voigt <andre.voigt@innogames.com>
2018-01-27 22:05:33 +0100

    Healthbar

--- js/entity/gamecontroller.js
+++ js/entity/gamecontroller.js
@@ -1,29 +1,30 @@
 define(['basic/entity','geo/v2', 'entity/clock'],
-function(Entity, V2, Clock) {
-    function GameController(pos) {
-      Entity.call(this);
+    function(Entity, V2, Clock) {
+        function GameController(pos) {
+            Entity.call(this);
 
-      this.position = pos;
-      this.current_time = 0;
-      this.current_seconds = 0;
-      this.current_minute = 0;
-      this.clock = new Clock(new V2(50, 50), this.current_seconds, this.current_minute);
-      this.add(this.clock);
+            this.position = pos;
+            this.current_time = 0;
+            this.current_seconds = 0;
+            this.current_minute = 0;
+            this.clock = new Clock(new V2(50, 50), this.current_seconds, this.current_minute);
+            this.add(this.clock);
 
-      this.currentScore = 0;
-      this.mulitplierer = 1;
-    };
+            this.currentScore = 0;
+            this.mulitplierer = 1;
+        };
 
-    GameController.prototype = new Entity();
+        GameController.prototype = new Entity();
 
-    GameController.prototype.onUpdate = function (delta) {
-      this.current_time += delta/1000;
-      this.current_seconds = Math.floor(this.current_time)%60;
-      this.current_minute = Math.floor(this.current_time)/60;
-      this.clock.text.text = this.clock.setClock(Math.floor(this.current_seconds), Math.floor(this.current_minute));
-      this.clock.text2.text = this.currentScore;
-      this.clock.text3.text = '+=' + this.mulitplierer;
-    };
-    return GameController;
-  }
+        GameController.prototype.onUpdate = function (delta) {
+            this.current_time += delta/1000;
+            this.current_seconds = Math.floor(this.current_time)%60;
+            this.current_minute = Math.floor(this.current_time)/60;
+            this.clock.text.text = this.clock.setClock(Math.floor(this.current_seconds), Math.floor(this.current_minute));
+            this.clock.text2.text = this.currentScore;
+            this.clock.text3.text = '+=' + this.mulitplierer;
+        };
+
+        return GameController;
+    }
 );
--- js/entity/healthbarcontroller.js
+++ js/entity/healthbarcontroller.js
@@ -1,36 +1,100 @@
 define(['basic/entity', 'geo/v2', 'core/graphic', 'basic/image'],
 	function(Entity, V2, Graphic, Image) {
-        var imageUrl = 'img/health.png';
-        Graphic.add(imageUrl);
-
+		var imageHealthbarUrl = 'img/UI.png';
+		var imageLives1Url = 'img/lives_1.png';
+		var imageLives2Url = 'img/lives_2.png';
+		var imageLives3Url = 'img/lives_3.png';
+		var imageHealthFullUrl = 'img/health_full.png';
+		var imageHealthMediumUrl = 'img/health_medium.png';
+		var imageHealthLowUrl = 'img/health_low.png';
+		Graphic.add(imageHealthbarUrl);
+		Graphic.add(imageLives1Url);
+		Graphic.add(imageLives2Url);
+		Graphic.add(imageLives3Url);
+		Graphic.add(imageHealthFullUrl);
+		Graphic.add(imageHealthMediumUrl);
+		Graphic.add(imageHealthLowUrl);
+		
 		function HealthbarController(pos) {
 			Entity.call(this);
 			this.position = pos;
 			this.initial_health = 5;
 			this.health = this.initial_health;
+			this.add(new Image(new V2(900, 500), imageHealthbarUrl));
+			this.health_blobs = [];
+			this.imageLives = '';
 
-			this.reset();
+			this.setTo(5, 3);
 		};
 
 		HealthbarController.prototype = new Entity();
 
 		HealthbarController.prototype.onUpdate = function (delta) {
-			var entities_length = this.entities.length;
-			if (this.health < entities_length) {
-				this.remove(this.entities[0]);
+			var health_blobs_length = this.health_blobs.length;
+			if (this.health < health_blobs_length) {
+				this.remove(this.health_blobs.pop());
 			}
+
+			this.setTo(this.health_blobs.length);
 		};
 
 		HealthbarController.prototype.reduce = function(value) {
 			this.health -= 1;
 		};
 
-		HealthbarController.prototype.reset = function() {
-			this.health = this.initial_health;
-			for (var i = 0; i < this.initial_health; i++) {
-				this.add(new Image(new V2(1000 + i * 45, 650), imageUrl));
+		HealthbarController.prototype.setTo = function(value, lives) {
+			var health_blob, image, image_lives = '', offset;
+
+			if (lives === 3) {
+				image_lives = imageLives3Url;
 			}
+			else if (lives === 2) {
+				image_lives = imageLives2Url;
+			}
+			else if (lives === 1) {
+				image_lives = imageLives1Url;
+			}
+
+			if (value === 5) {
+				image = imageHealthFullUrl;
+				offset = 1;
+			}
+			else if (value === 3) {
+				image = imageHealthMediumUrl;
+				offset = 3;
+			}
+			else if (value === 1) {
+				image = imageHealthLowUrl;
+				offset = 5;
+			}
+			else {
+				return;
+			}
+
+			this.removeHealthBlobs();
+			
+			for (var i = value - 1; i >= 0; --i) {
+				health_blob = new Image(new V2(883 + (i + offset) * 58, 606), image);
+				this.add(health_blob);
+				this.health_blobs.push(health_blob);
+			}
+
+			if (image_lives !== '') {
+				this.remove(this.imageLives);
+				this.imageLives = new Image(new V2(956, 518), image_lives);
+				this.add(this.imageLives);
+			}
+
+			this.health = value;
 		};
+
+		HealthbarController.prototype.removeHealthBlobs = function() {
+			for (var i = 0; i < this.health_blobs.length; i++) {
+				this.remove(this.health_blobs[i]);
+			}
+
+			this.health_blobs = [];
+		}
 
 		return HealthbarController;
 	}
