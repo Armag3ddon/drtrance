811ec83252819351c4ef18d710b6832301e7ed4e
andre.voigt <andre.voigt@innogames.com>
2018-01-27 16:21:07 +0100

    Menu

--- js/scenes/credits.js
+++ js/scenes/credits.js
@@ -2,10 +2,11 @@ define(['lib/scene', 'entity/back', 'basic/text', 'geo/v2'],
 		function(Scene, BackButton, TextEntity, V2) {
 			function CreditsScene() {
 				Scene.call(this);
-				this.center(new TextEntity(new V2(0, 100), "Max Mustermann"));
-				this.center(new TextEntity(new V2(0, 200), "Erica Mustemann"));
-				this.center(new TextEntity(new V2(0, 300), "Gunda Gamedesigner"));
-				this.center(new TextEntity(new V2(0, 400), "Peter Programmierer"));
+				this.center(new TextEntity(new V2(0, 100), ""));
+				this.center(new TextEntity(new V2(0, 200), ""));
+				this.center(new TextEntity(new V2(0, 300), ""));
+				this.center(new TextEntity(new V2(0, 400), ""));
+				this.center(new TextEntity(new V2(0, 500), ""));
 				this.center(BackButton('menu'));
 			}
 
